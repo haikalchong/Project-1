35b2e3549346c8235c468612702780a18c6dcb6a
haikalchong <haikalgg@gmail.com>
2023-02-15 15:13:32 +0000

    made function for random coordinates to work

--- map.js
+++ map.js
@@ -4,6 +4,7 @@
 
 
 
+
 // for map 
 const singapore = [1.3521, 103.8198]
 const accessTokenOne = "MJUzfOEpH1UHQFI8J2srmQ0g1WOuaiuOFyUKAT8n7hVn5vd9L7nCEtSaUybOaZHQ"
@@ -19,10 +20,10 @@ L.tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-toke
     accessToken: accessTokenOne
 }).addTo(map);
 
-async function getPokemon(){
-    let pokemon= Math.floor(Math.random() * 150) + 1
+async function getPokemon() {
+    let pokemon = Math.floor(Math.random() * 150) + 1
     let randomPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    
+
     return randomPokemon.data.name
 }
 
@@ -30,16 +31,49 @@ async function getPokemon(){
 
 
 //generate multiple pokemon at once
-async function multiplePokemon(){
-    for (i=0; i<5; i++){
-        let pokemonName= await getPokemon()
-        
+async function multiplePokemon() {
+    for (i = 0; i < 4; i++) {
+        let pokemonName = await getPokemon()
+
         console.log(pokemonName)
     }
-    
-}
-multiplePokemon()
 
-for (i=0; i<250; i++){
-console.log(Math.floor(Math.random()*150)+1)
 }
+
+//to generate random postal
+
+
+
+//generate random marker
+
+function marker(pokemon) {
+    //let postalCode=randomPost
+
+    // let randomMarker= await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${markerCoords}&returnGeom=Y&getAddrDetails=Y`)
+    for (i of pokemon) {
+        let lat = i.results[0].LATITUDE
+        let long = i.results[0].LONGITUDE
+        L.marker([lat, long]).addTo(map)
+    }
+}
+
+
+// marker(pokemonCoords)
+async function randomPost() {
+
+
+    let postalAr = []
+    while (postalAr.length < 6) {
+        let randomNumber = Math.floor(Math.random() * 900000) + 100000;
+        let randomCoords = await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${randomNumber}&returnGeom=Y&getAddrDetails=Y`)
+        if (randomCoords.data.found == 1) {
+            postalAr.push(randomCoords.data)
+        }
+
+
+
+    }
+    marker(postalAr)
+
+}
+randomPost()
