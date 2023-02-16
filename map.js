caf7a968bbf50983cef1ee8eaa1342794db12a42
haikalchong <haikalgg@gmail.com>
2023-02-16 08:56:18 +0000

    added interval to spawn more Pokemon

--- map.js
+++ map.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 // for pokemon to generate random pokemon
 
 
@@ -53,8 +57,8 @@ async function marker(pokemon) {
     for (i of pokemon) {
         let lat = i.results[0].LATITUDE
         let long = i.results[0].LONGITUDE
-        let=pokemonLoc=L.marker([lat, long]).addTo(map)
-        let pokeData= await getPokemon()
+        let = pokemonLoc = L.marker([lat, long]).addTo(map)
+        let pokeData = await getPokemon()
         // pokemonLoc.bindPopup(`<div><h3>${pokeData.name}</h3>
         // <img src="${pokeData.sprites.front_shiny}"/>
         // <ul>
@@ -64,21 +68,21 @@ async function marker(pokemon) {
         // </ul>
         // <button>Capture</button>
         // </div>`)
-       pokemonLoc.bindPopup( `<div class="card" style="width:100%; height:100%; background-color:grey;">
-        <img src="${pokeData.sprites.front_shiny}" width="60px !important" class="cardImg" alt="${pokeData.name}"/>
+        pokemonLoc.bindPopup(`<div class="card" style="width:100%; height:100%; background-color:black;">
+        <img src="${pokeData.sprites.front_shiny}" width="100px !important" class="cardImg" alt="${pokeData.name}"/>
         <div class="card-body">
-          <h5 class="card-title">${pokeData.name}</h5>
+          <h5 class="card-title" style="color:white;">${pokeData.name}</h5>
           <p class="card-text">
-          <ul>
-          <li>Pokemon ID: ${pokeData.id}</li>
-          <li>Type: ${pokeData.types[0].type.name}</li>
-          <li>Weight: ${pokeData.weight}</li>
+          <ul style="color:white;">
+          <li style="color:white;">Pokemon ID: ${pokeData.id}</li>
+          <li style="color:white;">Type: ${pokeData.types[0].type.name}</li>
+          <li style="color:white;">Weight: ${pokeData.weight}</li>
           </ul>
           </p>
           <button class="btn btn-primary">Capture</button>
         </div>
       </div>`)
-        
+
 
 
 
@@ -88,23 +92,22 @@ async function marker(pokemon) {
 
 // marker(pokemonCoords)
 async function randomPost() {
-
-
     let postalAr = []
     while (postalAr.length < 10) {
-        let randomNumber = Math.floor(Math.random() * 900000) + 100000;
+        let min = 100000
+        let max = 899999
+        let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
         let randomCoords = await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${randomNumber}&returnGeom=Y&getAddrDetails=Y`)
         if (randomCoords.data.found == 1) {
             postalAr.push(randomCoords.data)
         }
 
-
-
     }
     marker(postalAr)
-    
-
 }
-randomPost()
+
+setInterval(function(){
+    randomPost();
+}, 12000)
 
 
