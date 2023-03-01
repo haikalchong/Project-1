ceeedc2b27de1eb00eea4fd95bf5ba4bcb919e36
haikalchong <haikalgg@gmail.com>
2023-03-01 15:46:18 +0000

    added one map search and made it functional. Pending design and also adding more detail to the popUp. Also pending foursquared images

--- map.js
+++ map.js
@@ -88,10 +88,10 @@ L.tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-toke
 }).addTo(map);
 
 //which pokemon generation
-let pokemonGen=document.querySelector("#pokemonGenSelect").value
+let pokemonGen = document.querySelector("#pokemonGenSelect").value
 
-function changePokemonGen(){
-    pokemonGen=document.querySelector("#pokemonGenSelect").value
+function changePokemonGen() {
+    pokemonGen = document.querySelector("#pokemonGenSelect").value
     pokeLayer.clearLayers()
     randomPost()
 }
@@ -100,27 +100,27 @@ function changePokemonGen(){
 
 
 async function getPokemon() {
-    let min=1
-    let max=1
-    if(pokemonGen=="genOne"){
-        min=1
-        max=151
+    let min = 1
+    let max = 1
+    if (pokemonGen == "genOne") {
+        min = 1
+        max = 151
     }
-    else if(pokemonGen=="genTwo"){
+    else if (pokemonGen == "genTwo") {
         min = 152
-        max=251
+        max = 251
     }
-    else if(pokemonGen=="genThree"){
-        min= 252
-        max=386
+    else if (pokemonGen == "genThree") {
+        min = 252
+        max = 386
     }
-    else{
-        min= 387
-        max= 493
+    else {
+        min = 387
+        max = 493
     }
-    let pokemon =  Math.floor(Math.random() * (max - min + 1) + min);
+    let pokemon = Math.floor(Math.random() * (max - min + 1) + min);
     let randomPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-  
+
     return randomPokemon.data
 
 }
@@ -141,6 +141,7 @@ async function multiplePokemon() {
 
 let pokeLayer = L.layerGroup()
 let searchResultCluster = L.markerClusterGroup()
+let positionCluster=L.markerClusterGroup()
 
 //generate random marker
 
@@ -220,7 +221,7 @@ async function marker(pokemon) {
             //creating the pokemon data
 
             let pokedexData = document.createElement("div");
-            
+
 
 
             let pokedexCard = document.createElement("div")
@@ -236,17 +237,17 @@ async function marker(pokemon) {
 
             let pokemonMoves = document.createElement("ul");
             pokemonMoves.className = "list-group "
-            
-            let pokemonMovesArray=pokeData.moves
 
-            for (let i = 0; i<4; i++){
-                let moveElement=document.createElement("li")
-                moveElement.className="list-group-item"
-                moveElement.innerHTML= `Skill:${pokemonMovesArray[i].move.name}`
+            let pokemonMovesArray = pokeData.moves
+
+            for (let i = 0; i < 4; i++) {
+                let moveElement = document.createElement("li")
+                moveElement.className = "list-group-item"
+                moveElement.innerHTML = `Skill:${pokemonMovesArray[i].move.name}`
                 pokemonMoves.appendChild(moveElement)
             }
 
-           
+
             pokedexCard.appendChild(pokemonMoves);
             pokedexData.appendChild(pokedexCard);
 
@@ -269,21 +270,25 @@ async function marker(pokemon) {
 
 
 
+async function searchPosition(){
+    let searchLocation = document.querySelector("#searchValue").value;
+    let result = await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${searchLocation}&returnGeom=Y&getAddrDetails=Y`)
+    return result.data
+}
 
 
 
 
 
-       
 
 
 
 
-    
 
 
 
-  
+
+
 
 
 
@@ -295,7 +300,7 @@ async function marker(pokemon) {
 async function randomPost() {
     let postalArray = []
     while (postalArray.length < 10) {
-        let randomData=Math.floor((Math.random()*10))
+        let randomData = Math.floor((Math.random() * 10))
         let min = 1
         let max = 516
         let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
@@ -305,18 +310,19 @@ async function randomPost() {
 
 
 
-        
+
     }
 
     marker(postalArray)
-    
-   
+    weather()
+
+
 
 }
-//  setInterval(function () {
-//      pokeLayer.clearLayers()
-//      randomPost();
-//  }, 10000)
+setInterval(function () {
+    pokeLayer.clearLayers()
+    randomPost();
+}, 10000)
 
 // four square add on
 const fourSquareKey = "fsq3JrEqE31l1oSQP3kdQjP7B/tDogA6mWBfGxVi+3mBKl8=";
@@ -346,10 +352,11 @@ async function searchData(query, currentL) {
 let searchResultLayer = L.layerGroup()
 
 
+
 document.querySelector("#searchBtn").addEventListener("click", async function () {
     // let searchValue = document.querySelector("#searchValue").value;
     // let searchResults = await searchData(searchValue);
-    searchResultLayer.clearLayers();
+    
 
     //geolocation
     let currentL = ""
@@ -363,84 +370,115 @@ document.querySelector("#searchBtn").addEventListener("click", async function ()
         //  .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
         //  L.circle(e.latlng, radius).addTo(map);
-        
-    }
+
+    } ;
 
     map.on('locationfound', onLocationFound);
+    let searchFood  = document.querySelector('#food').checked
+   
+    
 
-    let searchValue = document.querySelector("#searchValue").value;
-    let searchResults = await searchData(searchValue, currentL);
 
-    for (let result of searchResults.results) {
-
-        let coordinate = [result.geocodes.main.latitude, result.geocodes.main.longitude];
-        let searchMarker = L.marker(coordinate, { icon:generalMarker}).addTo(searchResultLayer);
+    if (searchFood == true) {
+        searchResultCluster.clearLayers()
         
-        searchResultLayer.addTo(searchResultCluster)
+        
+        let searchValue = document.querySelector("#searchValue").value;
+        let searchResults = await searchData(searchValue, currentL);
 
-        searchMarker.bindPopup(`<div>
+        for (let result of searchResults.results) {
+
+            let coordinate = [result.geocodes.main.latitude, result.geocodes.main.longitude];
+            let searchMarker = L.marker(coordinate, { icon: generalMarker }).addTo(searchResultCluster);
+
+            
+
+            searchMarker.bindPopup(`<div>
         <h3>${result.name}</h3><p>
         ${result.location.formatted_address}</p></div>`)
+        }
+        searchResultCluster.addTo(map)
+    } 
+   else {
+        positionCluster.clearLayers()
+       let result = await searchPosition(searchValue);
+       for ( position of result.results){
+        let coordinates=([position.LATITUDE, position.LONGITUDE])
+        let positionMarker = L.marker(coordinates).addTo(positionCluster)
+        positionMarker.bindPopup(`<div> <h6>${position.BUILDING}</h6>
+        <p>${position.ADDRESS}</p>`)
+       }
+        positionCluster.addTo(map)
+        //let result = await axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${searchValue}&returnGeom=Y&getAddrDetails=Y`)
+
     }
-    searchResultCluster.addTo(map)
-
-
-
 });
 
 let pokedexData = []
 
- randomPost()
+randomPost()
 
 
 // add weather api
-let weatherLayer= L.layerGroup()
-const weatherApi= 'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast'
-let weatherArray=[]
+let weatherLayer = L.layerGroup()
+const weatherApi = 'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast'
+let weatherArray = []
 
-async function weather(){
-    let response= await axios.get(weatherApi)
-    let weatherArea=response.data.area_metadata;
-    
-   
-    
-    for (let weather of response.data.items[0].forecasts){
+async function weather() {
+    let response = await axios.get(weatherApi)
+    let weatherArea = response.data.area_metadata;
+
+
+
+    for (let weather of response.data.items[0].forecasts) {
         weatherArray.push(weather)
     }
-    
-    for (let i = 0; i<weatherArray.length; i++){
-      weatherArea[i].forecast= weatherArray[i]
-        
- }
-    for (let loc of weatherArea ){
+
+    for (let i = 0; i < weatherArray.length; i++) {
+        weatherArea[i].forecast = weatherArray[i]
+
+    }
+    for (let loc of weatherArea) {
         let lat = loc.label_location.latitude;
-        let lng= loc.label_location.longitude;
+        let lng = loc.label_location.longitude;
 
 
-        if(loc.forecast.forecast == "Cloudy" || loc.forecast.forecast == 'Partly Cloudy (Day)' || loc.forecast.forecast == 'Partly Cloudy (Night)'){
-            console.log("test")
-            L.marker([lat, lng],{icon:cloudIcon}).addTo(weatherLayer)
+        if (loc.forecast.forecast == "Cloudy" || loc.forecast.forecast == 'Partly Cloudy (Day)' || loc.forecast.forecast == 'Partly Cloudy (Night)') {
+            
+            L.marker([lat, lng], { icon: cloudIcon }).bindPopup(`<h6> ${loc.name}</h6>`).addTo(weatherLayer)
         }
 
-        if(loc.forecast.forecast == "Fair & Warm" || loc.forecast.forecast == 'Fair(Day)' || loc.forecast.forecast == 'Fair(Night)'){
-            L.marker([lat, lng],{icon:sunIcon}).addTo(weatherLayer)
+        if (loc.forecast.forecast == "Fair & Warm" || loc.forecast.forecast == 'Fair(Day)' || loc.forecast.forecast == 'Fair(Night)') {
+            L.marker([lat, lng], { icon: sunIcon }).bindPopup(`<h6> ${loc.name}</h6>`).addTo(weatherLayer)
         }
 
-        if(loc.forecast.forecast == 'Light Showers' || loc.forecast.forecast == 'Showers' || loc.forecast.forecast == 'Moderate Rain' || loc.forecast.forecast == 'Light Rain'){
-            L.marker([lat, lng],{icon:rainIcon}).addTo(weatherLayer)
+        if (loc.forecast.forecast == 'Light Showers' || loc.forecast.forecast == 'Showers' || loc.forecast.forecast == 'Moderate Rain' || loc.forecast.forecast == 'Light Rain') {
+            L.marker([lat, lng], { icon: rainIcon }).bindPopup(`<h6> ${loc.name}</h6>`).addTo(weatherLayer)
         }
-        if (loc.forecast.forecast == 'Thundery Showers' || loc.forecast.forecast == 'Heavy Thundery Showers' || loc.forecast.forecast == ' Heavy Thundery Showers with Gusty Winds'){
-            L.marker([lat, lng],{icon:thunderIcon}).addTo(weatherLayer)
+        if (loc.forecast.forecast == 'Thundery Showers' || loc.forecast.forecast == 'Heavy Thundery Showers' || loc.forecast.forecast == ' Heavy Thundery Showers with Gusty Winds') {
+            L.marker([lat, lng], { icon: thunderIcon }).bindPopup(`<h6> ${loc.name}</h6>`).addTo(weatherLayer)
         }
         weatherLayer.addTo(map)
     }
-      
-   
- 
+
+
+
 }
 
+let baseLayers = {
 
-weather()
+
+};
+
+let overlays = {
+    'Weather': weatherLayer
+}
+L.control.layers(baseLayers, overlays).addTo(map);
+
+
+
+
+
 
 
 
